37f5cb6fed64d4539fd63418c88f5841ca9a485d
“notURandomDev” <1250901577@qq.com>
2025-04-05 12:14:49 +0800

    feat: transportation card

--- app/(tabs)/guide/transportation.tsx
+++ app/(tabs)/guide/transportation.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import CustomLink from 'components/CustomLink';
 import ButtonAllinOne from 'components/ButtonAllinOne';
 import { ServiceCard } from 'components/ServiceCard';
+import { Ionicons } from '@expo/vector-icons';
 
 const TRANSPORTATION_SERVICE_CARDS = [
   { label: '行李寄存', source: require('../../../assets/imgs/service-route.png') },
@@ -19,6 +20,8 @@ const TransportationTabView = () => {
       className="relative py-3">
       <View className="gap-4">
         <CustomLink title="智能地图" />
+        <RouteInput />
+        <CommutePlan />
       </View>
       <View className="gap-4">
         <CustomLink title="接驳服务" />
@@ -36,5 +39,49 @@ const TransportationTabView = () => {
     </ScrollView>
   );
 };
+
+const RouteInput = () => (
+  <View
+    style={{ borderRadius: 15, borderWidth: 2.5 }}
+    className="flex-row items-center gap-4 border-blue px-3 py-2">
+    <View className="flex-1 gap-1.5">
+      <View className="flex-row items-center gap-2 ">
+        <Ionicons name="ellipse" color="#89EF7A" />
+        <Text className="text-xl">杭州东站</Text>
+      </View>
+      <View style={{ borderWidth: 0.5, marginLeft: 20 }} className="border border-gray"></View>
+      <View className="flex-row items-center gap-2 ">
+        <Ionicons name="ellipse" color="#F66348" />
+        <Text className="text-xl text-gray-solid">杭州国际博览中心</Text>
+      </View>
+    </View>
+    <View>
+      <Ionicons name="swap-vertical-outline" size={24} />
+    </View>
+  </View>
+);
+
+const CommutePlan = () => (
+  <View style={{ borderRadius: 15 }} className="gap-2 bg-blue-faint px-4 py-2">
+    <Text className="text-xl">全程 22分钟</Text>
+    <View className="flex-row gap-2">
+      <View
+        className="flex-1 items-center rounded-lg py-1"
+        style={{ backgroundColor: '#1556F050' }}>
+        <Text className="text-lg text-white">6号线</Text>
+      </View>
+      <Ionicons name="walk" size={24} color="#1556F0" />
+    </View>
+    <Text className="text-gray-solid">共步行210米·4元·7站</Text>
+    <View className="items-center">
+      <View className="flex-row items-center gap-2">
+        <Ionicons name="ellipse" size={7} color="#1556F0" />
+        <Ionicons name="ellipse" size={7} color="#D9D9D9" />
+        <Ionicons name="ellipse" size={7} color="#D9D9D9" />
+        <Ionicons name="ellipse" size={7} color="#D9D9D9" />
+      </View>
+    </View>
+  </View>
+);
 
 export default TransportationTabView;
